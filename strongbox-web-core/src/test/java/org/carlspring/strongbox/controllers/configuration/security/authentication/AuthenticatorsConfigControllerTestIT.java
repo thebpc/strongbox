a7efce82a875162dd413c8965680ecd5c870b1f1
carlspring <carlspring@gmail.com>
2019-01-13 18:13:43 +0000

    SB-1323: Make the strongbox-web-core's tests work in parallel
    Disabled the AuthenticatorsConfigControllerTestIT.

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/configuration/security/authentication/AuthenticatorsConfigControllerTestIT.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/configuration/security/authentication/AuthenticatorsConfigControllerTestIT.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
